fe452afa1780f3fe7a40c2ea49380fcb06f8e291
dorincreciun <creciundorin25@gmail.com>
2025-07-28 09:22:39 +0300

    - Add environment variables for API configuration - Enhance `Header` component structure and styling - Introduce Prettier configuration for code formatting

--- src/shared/common/Header/ui/index.tsx
+++ src/shared/common/Header/ui/index.tsx
@@ -1,5 +1,29 @@
+import { Link } from 'react-router'
+
 export const Header = () => {
-    return <header>Header</header>
+	return (
+		<header className={'container-fluid'}>
+			<div className={'container'}>
+				<Link to={'/'}>Logo</Link>
+				<nav>
+					<ul>
+						<li>
+							<Link to={'/'}>Link</Link>
+						</li>
+						<li>
+							<Link to={'/'}>Link</Link>
+						</li>
+						<li>
+							<Link to={'/'}>Link</Link>
+						</li>
+						<li>
+							<Link to={'/'}>Link</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	)
 }
 
 Header.displaName = 'Header'
